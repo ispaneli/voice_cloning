f7d5c369c16152668968ecb4644b629b3ea11a9f
Medvate <ilia.bezverzhenko@mail.ru>
2020-06-14 20:29:20 +0300

    Rewritten main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from synthesizer.inference import Synthesizer
+from inspect import getsourcefile
 import encoder
 import vocoder
 from pathlib import Path
@@ -6,61 +7,78 @@ import numpy as np
 import sounddevice as sd
 import soundfile as sf
 import os
+import re
+
+MAIN_PATH = os.path.abspath(getsourcefile(lambda: None))[0:-7]
+PATH_TO_ENCODER_WEIGHTS = Path(MAIN_PATH + 'saved_models/pretrained_encoder.pt')
+PATH_TO_VOCODER_WEIGHTS = Path(MAIN_PATH + 'saved_models/pretrained_vocoder.pt')
+PATH_TO_SYNTHESIZER_WEIGHTS = Path(MAIN_PATH + 'synthesizer/saved_models/logs-pretrained/taco_pretrained')
+
+SPECIAL_SYMBOLS = [' ', '.', ',', '!', '?', ':', ';']
+SYNTHESIZER_MODEL = None
 
 
-def init_encoder():
-    path_to_weights = Path('./saved_models/pretrained_encoder.pt')
-    encoder.load_model(path_to_weights)
-
-
-def init_vocoder():
-    path_to_weights = Path('./saved_models/pretrained_vocoder.pt')
-    vocoder.load_model(path_to_weights)
-
-
-def add_real_utterance(wav):
-    if not encoder.is_loaded():
-        init_encoder()
-
-    encoder_wav = encoder.wav_preprocess(wav_array=wav)
-    embed = encoder.get_embedding(encoder_wav)
-
-    return embed
-
-
-def synthesize(synthesizer, embed):
-    # Synthesize the spectrogram
-    if synthesizer is None:
-        checkpoints_dir = Path('./synthesizer/saved_models/logs-pretrained/taco_pretrained')
-        synthesizer = Synthesizer(checkpoints_dir, low_mem=False)
-    if not synthesizer.is_loaded():
-        print(synthesizer.checkpoint_fpath)
-
-    MESSAGE = 'Hello!\nThis is a test message.\nI want to check how the main function works.\nThanks for your attention.\nBye!'
-
-    texts = [MESSAGE.replace('\n', ' '), '']
-
-    embeds = np.stack([embed] * len(texts))
-    specs = synthesizer.synthesize_spectrograms(texts, embeds)
-    breaks = [spec.shape[1] for spec in specs]
-    spec = np.concatenate(specs, axis=1)
-
-    return spec, breaks
-
-
-def play(wav, sample_rate):
+def play(wav_array):
     sd.stop()
-    sd.play(wav, sample_rate, blocking=True)
+    sd.play(wav_array, Synthesizer.sample_rate, blocking=True)
 
 
-def vocode(spec, breaks):
-    assert spec is not None
+def check_and_get_path(path_to_file: str, exists: bool) -> Path:
+    result = Path(path_to_file)
 
-    # Synthesize the waveform
+    if result.exists() is exists:
+        if result.is_file() is exists:
+            if result.match('*.wav'):
+                return result
+            else:
+                raise FileExistsError
+        else:
+            raise FileExistsError
+    else:
+        raise FileExistsError(f"This file does not exist.\nparams: {result}, exists={exists}")
+
+
+def check_and_get_message(message: str) -> str:
+    message = message.replace('\n', ' ')
+
+    for letter in message:
+        if not (re.search(r'[a-zA-z]', letter) or letter in SPECIAL_SYMBOLS):
+            raise ValueError
+
+    return message
+
+
+def process_of_encoder(wav_array):
+    if not encoder.is_loaded():
+        encoder.load_model(PATH_TO_ENCODER_WEIGHTS)
+
+    encoder_wav = encoder.wav_preprocess(wav_array=wav_array)
+    embedding = encoder.get_embedding(encoder_wav)
+
+    return embedding
+
+
+def process_of_synthesizer(embedding, message):
+    global SYNTHESIZER_MODEL
+
+    if SYNTHESIZER_MODEL is None:
+        SYNTHESIZER_MODEL = Synthesizer(PATH_TO_SYNTHESIZER_WEIGHTS, low_mem=False)
+
+    texts = [message, '']
+
+    embeds = np.stack([embedding] * len(texts))
+    specs = SYNTHESIZER_MODEL.synthesize_spectrograms(texts, embeds)
+    breaks = [spec.shape[1] for spec in specs]
+    spectrogram = np.concatenate(specs, axis=1)
+
+    return spectrogram, breaks
+
+
+def process_of_vocode(spectrogram, breaks):
     if not vocoder.is_loaded():
-        init_vocoder()
+        vocoder.load_model(PATH_TO_VOCODER_WEIGHTS)
 
-    wav = vocoder.get_waveform(spec)
+    wav = vocoder.get_waveform(spectrogram)
 
     # Add breaks
     b_ends = np.cumsum(np.array(breaks) * Synthesizer.hparams.hop_size)
@@ -69,38 +87,38 @@ def vocode(spec, breaks):
     breaks = [np.zeros(int(0.15 * Synthesizer.sample_rate))] * len(breaks)
     wav = np.concatenate([i for w, b in zip(wavs, breaks) for i in (w, b)])
 
-    # Play it
     wav = wav / np.abs(wav).max() * 0.97
-    play(wav, Synthesizer.sample_rate)
 
-    sf.write('result.wav', wav, 16_000)
-
-
-def voice_create(synthesizer, embed):
-    spec, breaks = synthesize(synthesizer, embed)
-    vocode(spec, breaks)
+    return wav
 
 
-def main(path_to_voice_sample: Path):
-    wav = Synthesizer.load_preprocess_wav(path_to_voice_sample)
-    embed = add_real_utterance(wav)
+def clone_voice(path_to_voice: str, message: str, path_to_result: str, play_result: bool = False) -> None:
+    path_to_voice = check_and_get_path(path_to_voice, exists=True)
+    message = check_and_get_message(message)
+    path_to_result = check_and_get_path(path_to_result, exists=False)
 
-    # synthesizer будет типа Synthesizer.
-    synthesizer = None
-    voice_create(synthesizer, embed)
+    wav_array = Synthesizer.load_preprocess_wav(path_to_voice)
+    embedding = process_of_encoder(wav_array)
+    spectrogram, breaks = process_of_synthesizer(embedding, message)
+    wav = process_of_vocode(spectrogram, breaks)
+
+    if play_result:
+        play(wav)
+
+    sf.write(path_to_result, wav, 16_000)
 
 
 if __name__ == '__main__':
     print('module main was started!')
-    main(Path('voice_for_test.wav'))
+    MESSAGE = 'Hello!\nThis is a test message.\nI want to check how the main function works.\nThanks for your attention.\nBye!'
+    clone_voice('voice_for_test.wav', MESSAGE, 'result.wav', play_result=True)
     print('done!')
 
-    path_to_result = '/home/ilya/PycharmProjects/Real-Time-Voice-Cloning/result.wav'
-    size = os.path.getsize(path_to_result)
+    size = os.path.getsize(Path('result.wav'))
 
-    if 450_000 < size < 500_000:
+    if 135_000 < size < 180_000:
         print(f'Файл result.wav нормальный по весу.')
-        os.remove(path_to_result)
+        os.remove(Path('result.wav'))
         print('Файл был успешно удален!')
     else:
         raise ValueError('Несоответсвующий вес:', size)
